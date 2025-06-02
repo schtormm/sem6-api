9d9b5b71af5d7c4f532ec0b7dc2f437a7059ef48
schtormm <storm.noordermeer@gmail.com>
2025-06-02 11:20:14 +0200

    try to configure openai package properly

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ from openai import AzureOpenAI
 from customTypes import AnswerRequest, ChatRequest
 from utils import convertRecommendationToModel
 
+# LLM settings
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+api_key = os.getenv("AZURE_OPENAI_KEY")
+api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-12-01-preview")
+
+
 app = FastAPI()
+
 
 @app.get("/")
 def hello_world():
@@ -63,16 +71,11 @@ def parse_answers(request: AnswerRequest):
     except json.JSONDecodeError as e:
             raise HTTPException(status_code=400, detail="Invalid JSON format")
 
-# LLM settings
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-subscription_key = os.getenv("AZURE_OPENAI_KEY")
-api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-12-01-preview")
 
 client = AzureOpenAI(
     api_version=api_version,
     azure_endpoint=endpoint,
-    api_key=subscription_key,
+    api_key=api_key,
 )
 
 with open("System_prompt.txt", "r", encoding="utf-8") as f:
